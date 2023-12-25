51ba083dc772491c745be2ea1c5d02dc1971568e
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2023-12-25 16:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const extendHex = (shortHex) => {
 	let i = shortHex.length-3; isCapital = true ;
 	var output = "#";
 	for (let i = 0; i < shortHex.length; i++){
-		let ascii = shortHex[i].charCodeAt(0);
+		let ascii = shortHex[i];
 		 if (ascii > "a" && ascii <"z")
 		 //if (ascii > "A" && ascii <"Z")//
 		  {
