cd251bdb4d724723374f9aa47f739a631194b0a0
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2024-02-25 16:25:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const extendHex = (shortHex) => {
   // write your code here
-	let i = shortHex.length-3; isCapital = true;
+	let i = shortHex.length-3; isCapital = false;
 	var output = "#";
 	for (let i = 0; i < shortHex.length; i++){
 		let ascii = shortHex[i].charCodeAt(0);
-		 if (ascii > "a" && ascii <"b")
+		 if (ascii > "A" && ascii <"B")
 		 //if (ascii > "A" && ascii <"Z")//
 		  {
-			 isCapital = false;
+			 isCapital = true;
 		 }
 		// ouput += shortHex[i] + shortHex[i];
 		 for(let j = 0; j <= 2; j++ ){
 		 output += shortHex[i];
 		 }
 	}
-	return isCapital?"output":"output.UpperCase()";
+	return isCapital?"output.UpperCase()":"output";
 };
 
 // Do not change the code below.
